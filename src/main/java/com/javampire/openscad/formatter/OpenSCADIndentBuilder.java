9b9e128b9417f3c4401ca43b4903ede8702eb24c
Ian Cavén <icaven@imageandvideo.com>
2024-06-11 12:24:04 -0700

    Restore the optional indentation of cascading transforms.

--- src/main/java/com/javampire/openscad/formatter/OpenSCADIndentBuilder.java
+++ src/main/java/com/javampire/openscad/formatter/OpenSCADIndentBuilder.java
@@ -30,16 +30,15 @@ public class OpenSCADIndentBuilder {
 
         if (parentType == BLOCK_OBJ && !BRACES_TOKENS.contains(elementType)) { // Indent all BLOCK_OBJ (surrounded by brackets) children by default
             return Indent.getNormalIndent();
-/*
         } else if (parentType == BUILTIN_OBJ && elementType != BUILTIN_OP && elementType != BUILTIN_OBJ_REF
-                && elementType != BUILTIN_OVERRIDABLE_OP_AS_FUNCTION_REF) {
+                && elementType != BUILTIN_OVERRIDABLE_OBJ_REF)
+        {
             // Indent statements in BUILTIN_OBJ without brackets
             if (settings.INDENT_CASCADING_TRANSFORMATIONS) {
                 return Indent.getNormalIndent();
             } else {
                 return Indent.getNoneIndent();
             }
-*/
         } else if (parentType == IF_OBJ && (elementType == BUILTIN_OBJ || elementType == FOR_OBJ)) { // Indent statements in IF_OBJ without brackets
             return Indent.getNormalIndent();
         } else if (MATHEMATICAL_EXPR.contains(parentType)) {
--- src/test/testData/openscad/formatter/IndentObjectsElements_result_default.scad
+++ src/test/testData/openscad/formatter/IndentObjectsElements_result_default.scad
@@ -1,10 +1,10 @@
 render()
-color("red")
-rotate([0, 45, 0])
-translate([-a - 20, -20, 20])
-cube([10, 10, 10]);
+    color("red")
+        rotate([0, 45, 0])
+            translate([-a - 20, -20, 20])
+                cube([10, 10, 10]);
 
 translate([10, 10, 10]) {
     color("green")
-    sphere(r = 10);
+        sphere(r = 10);
 }
